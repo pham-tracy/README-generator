8eb16159cdcf38a6d7630f2f041da756dc2b9e15
Tracy Pham <phamtracy95@gmail.com>
2022-10-13 09:38:54 -0700

    Added MVP for generating README

--- index.js
+++ index.js
@@ -1,13 +1,76 @@
 // TODO: Include packages needed for this application
+const fs = require("fs");
+const inquirer = require("inquirer");
+const generateMarkdown = require("./utils/generateMarkdown");
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+    type: "input",
+    message: "Title of Project",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "Description",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "Installation",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Usage",
+    name: "usage",
+  },
+  {
+    type: "list", // TODO change this to options
+    message: "License",
+    name: "license",
+    choices: ["MIT", "Apache"],
+  },
+  {
+    type: "input",
+    message: "Contributing",
+    name: "contributing",
+  },
+  {
+    type: "input",
+    message: "Tests",
+    name: "tests",
+  },
+  {
+    type: "input",
+    message: "GitHub username",
+    name: "github",
+  },
+  {
+    type: "input",
+    message: "Email address",
+    name: "email",
+  },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
-
+function writeToFile(fileName, data) {
+  inquirer
+    .prompt(questions)
+    .then((data) =>
+      fs.writeFile(fileName, generateMarkdown(data), (err) =>
+        err ? console.error(err) : console.log(data)
+      )
+    );
+}
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  writeToFile("README.md");
+}
 
 // Function call to initialize app
 init();
+
+// Move inquirer to then lines, and console error to the init function
+// call writeToFile init with data so that its separate
+// so that writeToFile is just the writing to file part
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,6 +1,23 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+// function renderLicenseBadge(license) {
+//   switch (license) {
+//     case "MIT":
+//     return;
+//     break;
+
+//     case 2: "",
+//     return
+//     break;
+
+//     case 3: "",
+//     return;
+//     break;
+
+//     default:
+//     return ""
+//   }
+// }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
@@ -14,6 +31,43 @@ function renderLicenseSection(license) {}
 function generateMarkdown(data) {
   return `# ${data.title}
 
+## Description
+
+${data.description}
+
+
+## Table of Contents
+
+
+## Installation
+
+${data.installation}
+
+
+## Usage
+
+${data.usage}
+
+
+## License
+
+${data.license} is used for this project. Please refer to the LICENSE file in the repository for more details.
+
+
+## Contributing
+${data.contributing}
+
+
+## Tests
+
+${data.tests}
+
+
+## Questions
+
+If you have any questions about this project, please visit my github here: https://www.github.com/${data.github}.
+
+For further questions, please email me at ${data.email}
 `;
 }
 
